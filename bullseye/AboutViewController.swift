1e6e1d8c7f25a7d6de3bb86bbf6f7f51c20a9f0e
Muhammad Alaraby <mohamedelaraby1995@gmail.com>
2019-06-04 06:06:43 -0700

    add info viewcontroller

---
+++ bullseye/AboutViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AboutViewController.swift
+//  bullseye
+//
+//  Created by wiston on 6/4/19.
+//  Copyright © 2019 IDev. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+
+
+}
