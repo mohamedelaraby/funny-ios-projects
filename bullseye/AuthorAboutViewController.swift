ec5e048463576b747e887612ce27b33ced6d4fc7
Muhammad Alaraby <mohamedelaraby1995@gmail.com>
2019-06-04 06:24:19 -0700

    about the author

---
+++ bullseye/AuthorAboutViewController.swift
@@ -0,0 +1,22 @@
+//
+//  AuthorAboutViewController.swift
+//  bullseye
+//
+//  Created by wiston on 6/4/19.
+//  Copyright © 2019 IDev. All rights reserved.
+//
+
+import UIKit
+
+class AuthorAboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func close () {
+        dismiss(animated: true, completion: nil)
+    }
+
+
+}
